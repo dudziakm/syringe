4ab9f1b9f347b05e8d8ccb5abbeda62e7b742792
Simon Colmer <simon.colmer@totaljobsgroup.com>
2015-04-17 15:55:34 +0100

    Added web layer

---
+++ src/Syringe.Web.IisConfig/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using IisConfiguration;
+
+namespace Syringe.Web.IisConfig
+{
+    public class Config : EnvironmentalConfig
+    {
+        public override string WebRoot
+        {
+            get
+            {
+                //TODO: fix this by the time it goes out for deploy
+                string target = System.Environment.CurrentDirectory;
+                target = target.Substring(0, target.IndexOf("src\\Syringe.Web.IisConfig", StringComparison.InvariantCultureIgnoreCase));
+                target += "src\\Syringe.Web";
+
+                return Path.GetFullPath(target);
+            }
+        }
+
+        public string SiteName
+        {
+            get
+            {
+                return "Syringe.Web";
+            }
+        }
+
+        public int PortNumber
+        {
+            get
+            {
+                return 1231;
+            }
+        }
+    }
+}
