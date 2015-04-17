7883d8665761326d6c8eefeeaec615cf1a55400e
Chris S <mrshrinkray@gmail.com>
2015-04-17 17:07:35 +0100

    TestcaseReader tess

---
+++ src/Syringe.Core/Xml/TestCaseReader.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+using Syringe.Core.Exceptions;
+
+namespace Syringe.Core.Xml.LegacyConverter
+{
+    public class TestCaseReader : ITestCaseReader
+    {
+		public TestCaseCollection Read(TextReader textReader)
+		{
+			var testCollection = new TestCaseCollection();
+            XDocument doc = XDocument.Load(textReader);
+
+			// Check for <testcases>
+			XElement rootElement = doc.Elements().FirstOrDefault(i => i.Name.LocalName == "testcases");
+			if (rootElement == null)
+				throw new TestCaseException("<testcases> node is missing from the config file.");
+
+			// Repeats
+			int repeatValue = 0;
+			string repeatAttribute = XmlHelper.GetOptionalAttribute(rootElement, "repeat");
+			int.TryParse(repeatAttribute, out repeatValue);
+			testCollection.Repeat = repeatValue;
+
+			// <testvar>
+			testCollection.Variables = GetTestVars(rootElement);
+
+			// <case> - add each  one and re-order them by their id="" attribute.
+			var testCases = new List<TestCase>();
+			foreach (XElement element in rootElement.Elements().Where(x => x.Name.LocalName == "case"))
+			{
+				TestCase testCase = GetTestCase(element);
+				testCases.Add(testCase);
+			}
+			testCollection.TestCases = testCases.OrderBy(x => x.Id);
+
+			return testCollection;
+		}
+
+		private Dictionary<string, string> GetTestVars(XElement rootElement)
+		{
+            // <variables>
+            //      <variable name="login"></variable>
+            // </variables>
+
+			var variables = new Dictionary<string, string>();
+		    var variableElement = rootElement.Elements().Where(x => x.Name.LocalName == "variables");
+
+            foreach (XElement element in variableElement.Elements().Where(x => x.Name.LocalName == "variable"))
+			{
+				XAttribute varnameAttribute = element.Attributes("name").FirstOrDefault();
+				if (varnameAttribute != null)
+				{
+					if (!variables.ContainsKey(varnameAttribute.Value))
+					{
+						variables.Add(varnameAttribute.Value, element.Value);
+					}
+				}
+			}
+
+			return variables;
+		}
+
+		private TestCase GetTestCase(XElement element)
+		{
+			var testCase = new TestCase();
+
+			// Required Properties
+			testCase.Id = XmlHelper.AttributeAsInt(element, "id");
+			testCase.Url = XmlHelper.GetOptionalAttribute(element, "url");
+			if (string.IsNullOrEmpty(testCase.Url))
+				throw new TestCaseException("The url parameter is missing for test case {0}", testCase.Id);
+
+			// Optionals
+			testCase.Method = XmlHelper.GetOptionalAttribute(element, "method", "get");
+			testCase.PostBody = XmlHelper.GetOptionalElementValue(element, "postbody").Trim();
+			testCase.ErrorMessage = XmlHelper.GetOptionalAttribute(element, "errormessage");
+			testCase.PostType = XmlHelper.GetOptionalAttribute(element, "posttype", "application/x-www-form-urlencoded");
+			testCase.VerifyResponseCode = GetVerifyResponseCode(element);
+			testCase.LogRequest = YesToBool(element, "logrequest");
+			testCase.LogResponse = YesToBool(element, "logresponse");
+			testCase.Sleep = XmlHelper.AttributeAsInt(element, "sleep");
+			testCase.AddHeader = GetHeader(element);
+
+			// Numbered attributes
+			testCase.Descriptions = new List<NumberedAttribute>() { new NumberedAttribute(1, "shortdescription", "value")};
+            testCase.ParseResponses = GetElementCollection(element, "parseresponse", "parseresponses", "parseresponse");
+            testCase.VerifyPositives = GetElementCollection(element, "verifypositive", "verifypositives", "verify");
+            testCase.VerifyNegatives = GetElementCollection(element, "verifynegative", "verifynegatives", "verify");
+
+            // TODO: change descriptions to LongDescription, ShortDescription
+            // TODO: Rename/refactor numberedattribute
+            // TODO: Change AddHeader to Headers
+            // TODO: allow strings for ids.
+
+			return testCase;
+		}
+
+		private List<KeyValuePair<string, string>> GetHeader(XElement caseElement)
+		{
+			var headers = new List<KeyValuePair<string, string>>();
+
+            var variableElement = caseElement.Elements().Where(x => x.Name.LocalName == "headers");
+
+            foreach (XElement element in variableElement.Elements().Where(x => x.Name.LocalName == "header"))
+            {
+                XAttribute nameAttribute = element.Attributes("name").FirstOrDefault();
+                if (nameAttribute != null)
+                {
+                    headers.Add(new KeyValuePair<string, string>(nameAttribute.Value, element.Value));
+                }
+            }
+
+			return headers;
+		}
+
+		private bool YesToBool(XElement element, string attributeName)
+		{
+			string attributeValue = XmlHelper.GetOptionalAttribute(element, attributeName, "yes");
+			return (attributeValue == "yes");
+		}
+
+		private HttpStatusCode GetVerifyResponseCode(XElement caseElement)
+		{
+			int attributeValue = XmlHelper.AttributeAsInt(caseElement, "verifyresponsecode", 200);
+
+			HttpStatusCode statusCode = HttpStatusCode.OK;
+			if (Enum.IsDefined(typeof(HttpStatusCode), attributeValue))
+			{
+				Enum.TryParse(attributeValue.ToString(), out statusCode);
+			}
+
+			return statusCode;
+		}
+
+        internal List<NumberedAttribute> GetElementCollection(XElement caseElement, string name, string parentElementName, string childElementName)
+		{
+            if (string.IsNullOrEmpty(childElementName))
+                return new List<NumberedAttribute>();
+
+            var variables = new List<NumberedAttribute>();
+            var variableElement = caseElement.Elements().Where(x => x.Name.LocalName == parentElementName);
+
+		    int counter = 0;
+            foreach (XElement element in variableElement.Elements().Where(x => x.Name.LocalName == childElementName))
+            {
+                XAttribute idAttribute = element.Attributes("id").FirstOrDefault();
+                string id = counter.ToString();
+
+                if (idAttribute != null)
+                    id = idAttribute.Value;
+
+                variables.Add(new NumberedAttribute(counter, name, element.Value));
+
+                counter++;
+            }
+
+		    return variables;
+		}
+	}
+}
---
+++ src/Syringe.Tests/Unit/Xml/LegacyTestCaseReaderTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Syringe.Core;
+using Syringe.Core.Exceptions;
+using Syringe.Core.Xml.LegacyConverter;
+
+namespace Syringe.Tests.Unit.Xml.LegacyConverter
+{
+    public class LegacyTestCaseReaderTests : TestCaseReaderTests
+	{
+        protected override string XmlExamplesFolder
+        {
+            get { return "Syringe.Tests.Unit.Xml.LegacyXmlExamples."; }
+        }
+
+        protected override ITestCaseReader GetReader()
+        {
+            return new LegacyTestCaseReader();
+        }
+
+        [Test]
+        public void Read_should_cleanse_invalid_xml()
+        {
+            // Arrange
+            string xml = ReadEmbeddedFile("invalid-xml.xml");
+            var stringReader = new StringReader(xml);
+            var testCaseReader = GetReader();
+
+            // Act + Assert
+            testCaseReader.Read(stringReader);
+        }
+
+        [Test]
+        public void Read_should_parse_empty_addheader()
+        {
+            // Arrange
+            string xml = GetSingleCaseExample();
+            xml = xml.Replace("mykey: 12345|bar: foo|emptyvalue:|Cookie: referer=harrispilton.com", "");
+            var stringReader = new StringReader(xml);
+            var testCaseReader = GetReader();
+
+            // Act
+            TestCaseCollection testCollection = testCaseReader.Read(stringReader);
+
+            // Assert
+            TestCase testcase = testCollection.TestCases.First();
+            Assert.That(testcase.AddHeader.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Read_should_parse_single_addheader_attribute()
+        {
+            // Arrange
+            string xml = GetSingleCaseExample();
+            xml = xml.Replace("mykey: 12345|bar: foo|emptyvalue:|Cookie: referer=harrispilton.com", "User-Agent: Mozilla/5.0");
+            var stringReader = new StringReader(xml);
+            var testCaseReader = GetReader();
+
+            // Act
+            TestCaseCollection testCollection = testCaseReader.Read(stringReader);
+
+            // Assert
+            TestCase testcase = testCollection.TestCases.First();
+            Assert.That(testcase.AddHeader.Count, Is.EqualTo(1));
+            Assert.That(testcase.AddHeader[0].Key, Is.EqualTo("User-Agent"));
+            Assert.That(testcase.AddHeader[0].Value, Is.EqualTo("Mozilla/5.0"));
+        }
+
+        [Test]
+        public void GetNumberedAttributes_should_return_attributes_ordered_numerically()
+        {
+            // Arrange
+            string xml = ReadEmbeddedFile("ordered-attributes.xml");
+            XDocument document = XDocument.Parse(xml);
+            var firstTestCase = document.Root.Elements().First(x => x.Name.LocalName == "case");
+
+            var testCaseReader = new LegacyTestCaseReader();
+
+            // Act
+            List<NumberedAttribute> descriptions = testCaseReader.GetNumberedAttributes(firstTestCase, "description");
+
+            // Assert
+            Assert.That(descriptions[1].Name, Is.EqualTo("description"));
+            Assert.That(descriptions[0].Index, Is.EqualTo(0));
+            Assert.That(descriptions[0].Value, Is.EqualTo("description with no number"));
+
+            Assert.That(descriptions[1].Name, Is.EqualTo("description"));
+            Assert.That(descriptions[1].Index, Is.EqualTo(1));
+            Assert.That(descriptions[1].Value, Is.EqualTo("description 1"));
+
+            Assert.That(descriptions[2].Name, Is.EqualTo("description"));
+            Assert.That(descriptions[2].Index, Is.EqualTo(2));
+            Assert.That(descriptions[2].Value, Is.EqualTo("description 2"));
+
+            Assert.That(descriptions[3].Name, Is.EqualTo("description"));
+            Assert.That(descriptions[3].Index, Is.EqualTo(99));
+            Assert.That(descriptions[3].Value, Is.EqualTo("description 99"));
+        }
+	}
+}
