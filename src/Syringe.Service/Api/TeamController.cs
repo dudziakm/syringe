3b926b58d89167f29c77190282b7bb0d428301ee
Simon Colmer <simon.colmer@totaljobsgroup.com>
2015-04-17 14:04:07 +0100

    Added Team API

---
+++ src/Syringe.Service/Api/TeamController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+namespace Syringe.Service.Api
+{
+    public class TeamController : ApiController
+    {
+        [Route("api/team/list")]
+        [HttpGet]
+        public IEnumerable<string> List()
+        {
+            return new []
+            {
+                "Spitfire",
+                "Loki",
+            };
+        }
+    }
+}
