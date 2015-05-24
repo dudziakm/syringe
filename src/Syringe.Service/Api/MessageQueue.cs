db9f26f19c19e19a95b17bda18fb013ab2b18aac
Chris S <mrshrinkray@gmail.com>
2015-05-24 16:53:20 +0100

    Missing files from VS's github plugin

---
+++ src/Syringe.Service/Api/MessageQueue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+namespace Syringe.Service.Api
+{
+	public class MessageQueue
+	{
+		public ConcurrentQueue<string> Queue { get; private set; }
+
+		public MessageQueue()
+		{
+			Queue = new ConcurrentQueue<string>();
+		}
+
+		public void Add(string message)
+		{
+			Queue.Enqueue(message);
+		}
+
+		public string GetNext()
+		{
+			string result = "";
+			if (Queue.TryDequeue(out result))
+			{
+				return result;
+			}
+			else
+			{
+				return "An error occurred";
+			}
+		}
+	}
+}
