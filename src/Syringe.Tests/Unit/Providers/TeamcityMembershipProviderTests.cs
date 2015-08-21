f383359c55432d4bb3a32274f22ab8c2b0888abf
Richard Wright <rich.s.wright@gmail.com>
2015-08-21 11:27:23 +0100

    initial commit for adding teamcity membership provider

---
+++ src/Syringe.Tests/Unit/Providers/TeamcityMembershipProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Syringe.Tests.Unit.Providers
+{
+    [TestFixture]
+    public  class TeamcityMembershipProviderTests
+    {
+        [Test]
+        public void should_return_team_for_username()
+        {
+            // given
+
+            // when
+
+            // then
+         //   Assert.That(result, Is.Not.Null);
+        }
+    }
+}
