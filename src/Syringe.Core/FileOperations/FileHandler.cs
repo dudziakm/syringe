d90fe27f59560002deecf390958d2da9ba7710dd
Hemang Rajyaguru <hemdagem@gmail.com>
2015-10-10 21:40:40 +0100

    extracted a lot of case repository read and write functions to FileHandler. Started adding tests to case repository

---
+++ src/Syringe.Core/FileOperations/FileHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Syringe.Core.Configuration;
+using Syringe.Core.Logging;
+
+namespace Syringe.Core.FileOperations
+{
+    public class FileHandler : IFileHandler
+    {
+        private readonly IApplicationConfiguration _appConfig;
+
+        public FileHandler(IApplicationConfiguration appConfig)
+        {
+            _appConfig = appConfig;
+        }
+
+        public FileHandler() : this(new ApplicationConfig()) { }
+
+        public string GetFullPath(string fileName, string teamName)
+        {
+            string fullPath = Path.Combine(_appConfig.TestCasesBaseDirectory, teamName, fileName);
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException("The test case path cannot be found", fileName);
+
+            return fullPath;
+        }
+
+        public string GetFullPath(string teamName)
+        {
+            string fullPath = Path.Combine(_appConfig.TestCasesBaseDirectory, teamName);
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException("The team full path cannot be found", teamName);
+
+            return fullPath;
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public bool WriteAllText(string path, string contents)
+        {
+            try
+            {
+                File.WriteAllText(path, contents);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                //todo log error
+                Log.Error(exception, exception.Message);
+            }
+
+            return false;
+        }
+
+        public IEnumerable<string> GetFileNames(string teamName)
+        {
+            foreach (string file in Directory.EnumerateFiles(teamName))
+            {
+                var fileInfo = new FileInfo(file);
+                yield return fileInfo.Name;
+            }
+        }
+    }
+}
--- src/Syringe.Tests/Unit/Repositories/CaseRepositoryTests.cs
+++ src/Syringe.Tests/Unit/Repositories/CaseRepositoryTests.cs
@@ -1,10 +1,52 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using NUnit.Framework;
+using Syringe.Core.FileOperations;
+using Syringe.Core.Repositories;
+using Syringe.Core.TestCases;
+using Syringe.Core.Xml.Reader;
+using Syringe.Core.Xml.Writer;
 
 namespace Syringe.Tests.Unit.Repositories
 {
     [TestFixture]
     public class CaseRepositoryTests
     {
+        private Mock<ITestCaseReader> _testCaseReader;
+        private Mock<ITestCaseWriter> _testCaseWriter;
+        private Mock<IFileHandler> _fileHandler;
+        private CaseRepository _caseRepository;
+        [SetUp]
+        public void Setup()
+        {
+            _testCaseReader = new Mock<ITestCaseReader>();
+            _testCaseWriter = new Mock<ITestCaseWriter>();
+            _fileHandler = new Mock<IFileHandler>();
+
+            _fileHandler.Setup(x => x.GetFullPath(It.IsAny<string>(), It.IsAny<string>())).Returns("path");
+            _fileHandler.Setup(x => x.ReadAllText(It.IsAny<string>())).Returns("<xml></xml>");
+            _testCaseReader.Setup(x => x.Read(It.IsAny<TextReader>())).Returns(new CaseCollection { TestCases = new List<Case> { new Case() } });
+            _caseRepository = new CaseRepository(_testCaseReader.Object, _testCaseWriter.Object, _fileHandler.Object);
+        }
+
+        [Test]
+        public void GetTestCase_should_throw_null_reference_exception_when_caseId_is_invalid()
+        {
+            _testCaseReader.Setup(x => x.Read(It.IsAny<TextReader>())).Returns(new CaseCollection());
+
+            Assert.Throws<NullReferenceException>(() => _caseRepository.GetTestCase(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()));
+        }
+
+        [Test]
+        public void GetTestCase_should_set_parent_filename_when_testcase_is_found()
+        {
+            var testCase = _caseRepository.GetTestCase("parentFileName", It.IsAny<string>(), It.IsAny<int>());
+
+            Assert.AreEqual("parentFileName", testCase.ParentFilename);
+        }
+
 
     }
 }
