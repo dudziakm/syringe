3114431cc68ef63aff14c909d4cece5394239990
Hemang Rajyaguru <hemdagem@gmail.com>
2015-11-15 21:30:08 +0000

    minor refactor

--- src/Syringe.Web/Scripts/Syringe/Testcase.ts
+++ src/Syringe.Web/Scripts/Syringe/Testcase.ts
@@ -9,19 +9,18 @@
 
     function appendDataItem(panelBody, html, elementPrefix) {
 
-        var currentRow = panelBody.find(".form-group:last-child"), rowNumber = 0;
+        var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
 
-        if (currentRow.length !== 0) {
-
-            var firstInputName = currentRow.find("input:first").attr("name");
+        if (formGroup.length !== 0) {
+            var firstInputName = formGroup.find("input:first").attr("name");
             
             // get the last index number of the row and increment it by 1
             rowNumber = parseInt(firstInputName.match(/\d/g)) + 1;
         }
 
         // replace the name value with the correct prefix and row number so it can be posted to the server 
-        var newData = html.replace(/name="/g, "name=\"" + elementPrefix + "[" + rowNumber + "].");
-        panelBody.append(newData);
+        var newHtml = html.replace(/name="/g, "name=\"" + elementPrefix + "[" + rowNumber + "].");
+        panelBody.append(newHtml);
     }
 
     function setupButtons() {
