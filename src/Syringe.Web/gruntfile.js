31894c831d84a64f360fa095b13682abd63c5798
Chris <mrshrinkray@gmail.com>
2015-11-16 14:24:26 +0000

    Added SASS compilation through Node/Grunt. Also I shouldn't have ignored .js files

---
+++ src/Syringe.Web/gruntfile.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/Syringe.css': 'css/Syringe.scss'
+                }
+            }
+        },
+        watch: {
+
+            css: {
+                files: ['scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["sasscompile"]);
+
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+};
