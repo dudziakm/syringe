6a06c425e0214b086074831df2f66c290830d3e3
Hemang Rajyaguru <hemdagem@gmail.com>
2015-11-16 00:00:17 +0000

    use TypeScript for TestCase

--- src/Syringe.Web/Scripts/Syringe.js
+++ src/Syringe.Web/Scripts/Syringe.js
@@ -175,58 +175,53 @@ var Syringe;
         Web.TestCaseRunner = TestCaseRunner;
     })(Web = Syringe.Web || (Syringe.Web = {}));
 })(Syringe || (Syringe = {}));
-(function () {
-    var jQueryElements = {
-        addVerificationButton: $("#addVerification"),
-        addParsedItemButton: $("#addParsedItem"),
-        addHeaderItemButton: $("#addHeaderItem"),
-        addVariableItemButton: $("#addVariableItem")
-    };
-    function appendDataItem(panelBody, html, elementPrefix) {
-        var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
-        if (formGroup.length !== 0) {
-            var firstInputName = formGroup.find("input:first").attr("name");
-            // get the last index number of the row and increment it by 1
-            rowNumber = parseInt(firstInputName.match(/\d/g)) + 1;
-        }
-        // replace the name value with the correct prefix and row number so it can be posted to the server 
-        var newHtml = html.replace(/name="/g, "name=\"" + elementPrefix + "[" + rowNumber + "].");
-        panelBody.append(newHtml);
+var RowHandler = (function () {
+    function RowHandler(i) {
+        var _this = this;
+        this.addRow = function (testCase) {
+            $.get(testCase.URL, function (html) {
+                var panelBody = testCase.$Button.parent().next();
+                var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
+                if (formGroup.length !== 0) {
+                    var firstInputName = formGroup.find("input:first").attr("name");
+                    // get the last index number of the row and increment it by 1
+                    rowNumber = parseInt(firstInputName.match(/\d/g)) + 1;
+                }
+                // replace the name value with the correct prefix and row number so it can be posted to the server 
+                var newHtml = html.replace(/name="/g, "name=\"" + testCase.Prefix + "[" + rowNumber + "].");
+                panelBody.append(newHtml);
+            });
+        };
+        this.getData = function (e) {
+            e.preventDefault();
+            _this.addRow(e.data.testCase);
+        };
+        this.setupButtons = function () {
+            for (var iTestCase in _this.rowsToAdd) {
+                if (_this.rowsToAdd.hasOwnProperty(iTestCase)) {
+                    var testCase = _this.rowsToAdd[iTestCase];
+                    testCase.$Button.on("click", { testCase: testCase }, _this.getData);
+                }
+            }
+        };
+        this.rowsToAdd = i;
     }
-    function setupButtons() {
-        jQueryElements.addVerificationButton.click(function (e) {
-            e.preventDefault();
-            $.get("/TestCase/AddVerification", function (data) {
-                appendDataItem(jQueryElements.addVerificationButton.parent().next(), data, "Verifications");
-            });
-        });
-        jQueryElements.addParsedItemButton.click(function (e) {
-            e.preventDefault();
-            $.get("/TestCase/AddParseResponseItem", function (data) {
-                appendDataItem(jQueryElements.addParsedItemButton.parent().next(), data, "ParseResponses");
-            });
-        });
-        jQueryElements.addHeaderItemButton.click(function (e) {
-            e.preventDefault();
-            $.get("/TestCase/AddHeaderItem", function (data) {
-                appendDataItem(jQueryElements.addHeaderItemButton.parent().next(), data, "Headers");
-            });
-        });
-        jQueryElements.addVariableItemButton.click(function (e) {
-            e.preventDefault();
-            $.get("/TestFile/AddVariableItem", function (data) {
-                appendDataItem(jQueryElements.addVariableItemButton.parent().next(), data, "Variables");
-            });
-        });
-        $("body").on("click", "#removeRow", function (e) {
-            e.preventDefault();
-            $(this).closest(".form-group").remove();
-        });
-    }
-    $(document).ready(function () {
-        setupButtons();
+    return RowHandler;
+})();
+$(document).ready(function () {
+    var i = [
+        { $Button: $("#addVerification"), URL: "/TestCase/AddVerification", Prefix: "Verifications" },
+        { $Button: $("#addParsedItem"), URL: "/TestCase/AddParseResponseItem", Prefix: "ParseResponses" },
+        { $Button: $("#addHeaderItem"), URL: "/TestCase/AddHeaderItem", Prefix: "Headers" },
+        { $Button: $("#addVariableItem"), URL: "/TestFile/AddVariableItem", Prefix: "Variables" }
+    ];
+    var rowHandler = new RowHandler(i);
+    rowHandler.setupButtons();
+    $("body").on("click", "#removeRow", function (e) {
+        e.preventDefault();
+        $(this).closest(".form-group").remove();
     });
-}());
+});
 var VerifyType;
 (function (VerifyType) {
     VerifyType[VerifyType["Negative"] = 0] = "Negative";
--- src/Syringe.Web/Scripts/Syringe/Testcase.ts
+++ src/Syringe.Web/Scripts/Syringe/Testcase.ts
@@ -1,70 +1,57 @@
-﻿(function () {
+﻿class RowHandler {
 
-    var jQueryElements = {
-        addVerificationButton: $("#addVerification"),
-        addParsedItemButton: $("#addParsedItem"),
-        addHeaderItemButton: $("#addHeaderItem"),
-        addVariableItemButton: $("#addVariableItem")
-    };
+    rowsToAdd: IRowAdder[];
+    constructor(i: IRowAdder[]) {
+        this.rowsToAdd = i;
+    }
 
-    function appendDataItem(panelBody, html, elementPrefix) {
+    private addRow = (testCase) => {
 
-        var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
+        $.get(testCase.URL, function (html) {
+            var panelBody = testCase.$Button.parent().next();
+            var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
 
-        if (formGroup.length !== 0) {
-            var firstInputName = formGroup.find("input:first").attr("name");
-            
-            // get the last index number of the row and increment it by 1
-            rowNumber = parseInt(firstInputName.match(/\d/g)) + 1;
+            if (formGroup.length !== 0) {
+                var firstInputName = formGroup.find("input:first").attr("name");
+
+                // get the last index number of the row and increment it by 1
+                rowNumber = parseInt(firstInputName.match(/\d/g)) + 1;
+            }
+
+            // replace the name value with the correct prefix and row number so it can be posted to the server 
+            var newHtml = html.replace(/name="/g, "name=\"" + testCase.Prefix + "[" + rowNumber + "].");
+            panelBody.append(newHtml);
+        });
+    }
+
+    private getData = (e) => {
+        e.preventDefault();
+        this.addRow(e.data.testCase);
+    }
+
+    public setupButtons = () => {
+        for (let iTestCase in this.rowsToAdd) {
+            if (this.rowsToAdd.hasOwnProperty(iTestCase)) {
+                var testCase = this.rowsToAdd[iTestCase];
+                testCase.$Button.on("click", { testCase: testCase }, this.getData);
+            }
         }
-
-        // replace the name value with the correct prefix and row number so it can be posted to the server 
-        var newHtml = html.replace(/name="/g, "name=\"" + elementPrefix + "[" + rowNumber + "].");
-        panelBody.append(newHtml);
     }
+}
 
-    function setupButtons() {
+$(document).ready(function () {
+    const i = [
+        { $Button: $("#addVerification"), URL: "/TestCase/AddVerification", Prefix: "Verifications" },
+        { $Button: $("#addParsedItem"), URL: "/TestCase/AddParseResponseItem", Prefix: "ParseResponses" },
+        { $Button: $("#addHeaderItem"), URL: "/TestCase/AddHeaderItem", Prefix: "Headers" },
+        { $Button: $("#addVariableItem"), URL: "/TestFile/AddVariableItem", Prefix: "Variables" }
+    ];
+    let rowHandler = new RowHandler(i);
+    rowHandler.setupButtons();
 
-        jQueryElements.addVerificationButton.click(function (e) {
-            e.preventDefault();
-
-            $.get("/TestCase/AddVerification", function (data) {
-                appendDataItem(jQueryElements.addVerificationButton.parent().next(), data, "Verifications");
-            });
-        });
-
-        jQueryElements.addParsedItemButton.click(function (e) {
-            e.preventDefault();
-
-            $.get("/TestCase/AddParseResponseItem", function (data) {
-                appendDataItem(jQueryElements.addParsedItemButton.parent().next(), data, "ParseResponses");
-            });
-        });
-
-        jQueryElements.addHeaderItemButton.click(function (e) {
-            e.preventDefault();
-
-            $.get("/TestCase/AddHeaderItem", function (data) {
-                appendDataItem(jQueryElements.addHeaderItemButton.parent().next(), data, "Headers");
-            });
-        });
-
-        jQueryElements.addVariableItemButton.click(function (e) {
-            e.preventDefault();
-
-            $.get("/TestFile/AddVariableItem", function (data) {
-                appendDataItem(jQueryElements.addVariableItemButton.parent().next(), data, "Variables");
-            });
-        });
-
-        $("body").on("click", "#removeRow", function (e) {
-            e.preventDefault();
-            $(this).closest(".form-group").remove();
-        });
-    }
-
-    $(document).ready(function () {
-        setupButtons();
+    $("body").on("click", "#removeRow", function (e) {
+        e.preventDefault();
+        $(this).closest(".form-group").remove();
     });
+});
 
-} ());
