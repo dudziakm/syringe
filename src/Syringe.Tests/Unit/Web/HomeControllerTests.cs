57200d775e3500970cd4f5eb50059c2d60d75bed
Hemang Rajyaguru <hemdagem@gmail.com>
2015-11-19 22:22:01 +0000

    added home controller tests

---
+++ src/Syringe.Tests/Unit/Web/HomeControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using Syringe.Core.Repositories;
+using Syringe.Core.Results;
+using Syringe.Core.Security;
+using Syringe.Core.Services;
+using Syringe.Web.Controllers;
+using Syringe.Web.Models;
+
+namespace Syringe.Tests.Unit.Web
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private Mock<ICaseService> _casesClient;
+        private Mock<IUserContext> _userContext;
+        private Mock<Func<IRunViewModel>> _runViewModelFactory;
+        private Mock<ITestCaseSessionRepository> _repository;
+        private Mock<Func<ICanaryService>> _canaryClientFactory;
+        private HomeController homeController;
+        [SetUp]
+        public void Setup()
+        {
+            _casesClient = new Mock<ICaseService>();
+            _userContext = new Mock<IUserContext>();
+            _repository = new Mock<ITestCaseSessionRepository>();
+            _canaryClientFactory = new Mock<Func<ICanaryService>>();
+            _runViewModelFactory = new Mock<Func<IRunViewModel>>();
+            var mockService = new Mock<IRunViewModel>();
+            mockService.Setup(x => x.Run(It.IsAny<UserContext>(), It.IsAny<string>()));
+            _runViewModelFactory.Setup(x => x()).Returns(mockService.Object);
+
+            _repository.Setup(x => x.GetById(It.IsAny<Guid>())).Returns(new TestCaseSession());
+
+            homeController = new HomeController(_casesClient.Object, _userContext.Object, _runViewModelFactory.Object,
+                _canaryClientFactory.Object, _repository.Object);
+        }
+
+        [Test]
+        public void AllResults_should_return_correct_view_and_model()
+        {
+            // given + when
+            var viewResult = homeController.AllResults() as ViewResult;
+
+            // then
+            _repository.Verify(x => x.GetSummaries(), Times.Once);
+            Assert.AreEqual("AllResults", viewResult.ViewName);
+            Assert.IsInstanceOf<IEnumerable<SessionInfo>>(viewResult.Model);
+        }
+
+        [Test]
+        public void TodaysResults_should_return_correct_view_and_model()
+        {
+            // given + when
+            var viewResult = homeController.TodaysResults() as ViewResult;
+
+            // then
+            _repository.Verify(x => x.GetSummariesForToday(), Times.Once);
+            Assert.AreEqual("AllResults", viewResult.ViewName);
+            Assert.IsInstanceOf<IEnumerable<SessionInfo>>(viewResult.Model);
+        }
+
+
+        [Test]
+        public void ViewResult_should_return_correct_view_and_model()
+        {
+            // given + when
+            var viewResult = homeController.ViewResult(It.IsAny<Guid>()) as ViewResult;
+
+            // then
+            _repository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Once);
+            Assert.AreEqual("ViewResult", viewResult.ViewName);
+            Assert.IsInstanceOf<TestCaseSession>(viewResult.Model);
+        }
+
+        [Test]
+        public async void DeleteResult_should_call_delete_methods_and_redirect_to_correct_action()
+        {
+            // given + when
+            var redirectToRouteResult = await homeController.DeleteResult(It.IsAny<Guid>()) as RedirectToRouteResult;
+
+            // then
+            _repository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Once);
+            _repository.Verify(x => x.DeleteAsync(It.IsAny<TestCaseSession>()), Times.Once);
+            Assert.AreEqual("AllResults", redirectToRouteResult.RouteValues["action"]);
+        }
+
+        [Test]
+        public void Run_should_call_run_method_and_return_correct_model()
+        {
+            // given + when
+            var viewResult = homeController.Run(It.IsAny<string>()) as ViewResult;
+
+            // then
+            _runViewModelFactory.Verify(x => x(), Times.Once);
+            Assert.AreEqual("Run", viewResult.ViewName);
+            Assert.IsInstanceOf<IRunViewModel>(viewResult.Model);
+        }
+    }
+}
--- src/Syringe.Web/Controllers/HomeController.cs
+++ src/Syringe.Web/Controllers/HomeController.cs
@@ -13,86 +13,86 @@ using Syringe.Web.Models;
 
 namespace Syringe.Web.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ICaseService _casesClient;
-		private readonly IUserContext _userContext;
-		private readonly Func<IRunViewModel> _runViewModelFactory;
-		private readonly ITestCaseSessionRepository _repository;
-		private readonly Func<ICanaryService> _canaryClientFactory;
+    public class HomeController : Controller
+    {
+        private readonly ICaseService _casesClient;
+        private readonly IUserContext _userContext;
+        private readonly Func<IRunViewModel> _runViewModelFactory;
+        private readonly ITestCaseSessionRepository _repository;
+        private readonly Func<ICanaryService> _canaryClientFactory;
 
-		public HomeController(
-			ICaseService casesClient,
-			IUserContext userContext,
-			Func<IRunViewModel> runViewModelFactory,
-			Func<ICanaryService> canaryClientFactory,
-			ITestCaseSessionRepository repository)
-		{
-			_casesClient = casesClient;
-			_userContext = userContext;
-			_runViewModelFactory = runViewModelFactory;
-			_canaryClientFactory = canaryClientFactory;
-			_repository = repository;
-		}
+        public HomeController(
+            ICaseService casesClient,
+            IUserContext userContext,
+            Func<IRunViewModel> runViewModelFactory,
+            Func<ICanaryService> canaryClientFactory,
+            ITestCaseSessionRepository repository)
+        {
+            _casesClient = casesClient;
+            _userContext = userContext;
+            _runViewModelFactory = runViewModelFactory;
+            _canaryClientFactory = canaryClientFactory;
+            _repository = repository;
+        }
 
-		public ActionResult Index(int pageNumber = 1, int noOfResults = 10)
-		{
-			CheckServiceIsRunning();
+        public ActionResult Index(int pageNumber = 1, int noOfResults = 10)
+        {
+            CheckServiceIsRunning();
 
-			// TODO: team name from the user context?
-			IList<string> files = _casesClient.ListFilesForTeam(_userContext.TeamName).ToList();
+            // TODO: team name from the user context?
+            IList<string> files = _casesClient.ListFilesForTeam(_userContext.TeamName).ToList();
 
-		    var model = new IndexViewModel
-		    {
-		        PageNumber = pageNumber,
-		        NoOfResults = noOfResults,
-		        PageNumbers = files.GetPageNumbersToShow(noOfResults),
-		        Files = files.GetPaged(noOfResults, pageNumber)
-		    };
+            var model = new IndexViewModel
+            {
+                PageNumber = pageNumber,
+                NoOfResults = noOfResults,
+                PageNumbers = files.GetPageNumbersToShow(noOfResults),
+                Files = files.GetPaged(noOfResults, pageNumber)
+            };
 
-		    return View(model);
-		}
+            return View("Index", model);
+        }
 
-		[HttpPost]
-		public ActionResult Run(string filename)
-		{
-			var runViewModel = _runViewModelFactory();
-			runViewModel.Run(_userContext, filename);
-			return View(runViewModel);
-		}
+        [HttpPost]
+        public ActionResult Run(string filename)
+        {
+            var runViewModel = _runViewModelFactory();
+            runViewModel.Run(_userContext, filename);
+            return View("Run", runViewModel);
+        }
 
-		private void CheckServiceIsRunning()
-		{
-			var canaryCheck = _canaryClientFactory();
-			CanaryResult result = canaryCheck.Check();
-			if (result == null || result.Success == false)
-			{
-				throw new InvalidOperationException("Unable to connect to the REST api service. Is the service started? Check it at http://localhost:8086/");
-			}
-		}
+        private void CheckServiceIsRunning()
+        {
+            var canaryCheck = _canaryClientFactory();
+            CanaryResult result = canaryCheck.Check();
+            if (result == null || result.Success == false)
+            {
+                throw new InvalidOperationException("Unable to connect to the REST api service. Is the service started? Check it at http://localhost:8086/");
+            }
+        }
 
-		public ActionResult AllResults()
-		{
-			return View("AllResults", _repository.GetSummaries());
-		}
+        public ActionResult AllResults()
+        {
+            return View("AllResults", _repository.GetSummaries());
+        }
 
-		public ActionResult TodaysResults()
-		{
-			return View("AllResults", _repository.GetSummariesForToday());
-		}
+        public ActionResult TodaysResults()
+        {
+            return View("AllResults", _repository.GetSummariesForToday());
+        }
 
-		public ActionResult ViewResult(Guid id)
-		{
-			return View("ViewResult", _repository.GetById(id));
-		}
+        public ActionResult ViewResult(Guid id)
+        {
+            return View("ViewResult", _repository.GetById(id));
+        }
 
-		[HttpPost]
-		public async Task<ActionResult> DeleteResult(Guid id)
-		{
-			TestCaseSession session = _repository.GetById(id);
-			await _repository.DeleteAsync(session);
+        [HttpPost]
+        public async Task<ActionResult> DeleteResult(Guid id)
+        {
+            TestCaseSession session = _repository.GetById(id);
+            await _repository.DeleteAsync(session);
 
-			return RedirectToAction("AllResults");
-		}
-	}
+            return RedirectToAction("AllResults");
+        }
+    }
 }
