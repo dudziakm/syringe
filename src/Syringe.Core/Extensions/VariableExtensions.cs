668ca9be9617640e44441b0f36a25075660d9363
Hemang Rajyaguru <hemdagem@gmail.com>
2015-11-19 22:53:06 +0000

    Revert "remove unused class"
    This reverts commit 09bba8921ea0883338c07af2aa5de9535546bc63.

---
+++ src/Syringe.Core/Extensions/VariableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syringe.Core.Configuration;
+using Syringe.Core.TestCases.Configuration;
+
+namespace Syringe.Core.Extensions
+{
+	public static class VariableExtensions
+	{
+		public static string ByName(this List<Variable> list, string name)
+		{
+			Variable item = list.FirstOrDefault(x => x.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+			if (item != null)
+			{
+				return item.Value;
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
