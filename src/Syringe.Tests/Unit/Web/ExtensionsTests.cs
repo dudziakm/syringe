aa9015cec78c6d55a78c4a83dfe41b5def1de40b
Hemang Rajyaguru <hemdagem@gmail.com>
2015-11-20 22:49:37 +0000

    added extensions tests

---
+++ src/Syringe.Tests/Unit/Web/ExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Syringe.Web.Extensions;
+
+namespace Syringe.Tests.Unit.Web
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+        [Test]
+        public void MinutesAndSecondsFormat_should_return_time_in_seconds_if_less_then_a_minute()
+        {
+            // given
+            var minutesAndSecondsFormat = new TimeSpan(0, 0, 0, 58).MinutesAndSecondsFormat();
+
+            Assert.AreEqual("58 seconds", minutesAndSecondsFormat);
+        }
+
+        [Test]
+        public void MinutesAndSecondsFormat_should_return_time_in_minutes_and_seconds_if_more_then_a_minute()
+        {
+            // given
+            var minutesAndSecondsFormat = new TimeSpan(0, 0, 1, 58).MinutesAndSecondsFormat();
+
+            Assert.AreEqual("1m 58s", minutesAndSecondsFormat);
+        }
+    }
+}
