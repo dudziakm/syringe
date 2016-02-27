991d2c9b2d158450f1b262da192996a6b2853add
Chris S <mrshrinkray@gmail.com>
2016-02-27 18:17:36 +0000

    Chocolatey push to MyGet via appveyor

---
+++ build/push-chocopackage.ps1
@@ -0,0 +1,20 @@
+param([string]$version, [string] $apiKey)
+
+$root = $(Split-Path -parent $MyInvocation.MyCommand.Source)
+$root = $(Split-Path -parent $root)
+
+$nuspecPath = "$root\src\Chocolatey\Syringe\syringe.nuspec"
+$installScriptPath = "$root\src\Chocolatey\Syringe\tools\chocolateyinstall.ps1"
+
+write-host "Nuspec path: $nuspecPath"
+$contents = [System.IO.File]::ReadAllText($nuspecPath)
+$contents = $contents.Replace('$version$', "$version")
+[System.IO.File]::WriteAllText($nuspecPath, $contents);
+
+write-host "chocolateyinstall.ps1 path: $installScriptPath"
+$contents = [System.IO.File]::ReadAllText($installScriptPath)
+$contents = $contents.Replace('{{VERSION}}', "$version")
+[System.IO.File]::WriteAllText($installScriptPath, $contents);
+
+choco pack "$root\src\Chocolatey\Syringe\syringe.nuspec"
+choco push Syringe.$version.nupkg --source https://www.myget.org/F/syringe/api/v2/package --api-key=$apiKey
