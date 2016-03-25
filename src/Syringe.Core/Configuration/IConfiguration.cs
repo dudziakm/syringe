e670e1293d7b0491f16f91092068d70464553bbc
Chris S <mrshrinkray@gmail.com>
2016-03-25 23:39:05 +0000

    Added missing file - Visual Studio Git integration didn't see IConfiguration

---
+++ src/Syringe.Core/Configuration/IConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Syringe.Core.Configuration
+{
+	public interface IConfiguration
+	{
+		string ServiceUrl { get; set; }
+		string WebsiteUrl { get; set; }
+		string TestCasesBaseDirectory { get; set; }
+		OAuthConfiguration OAuthConfiguration { get; set; }
+		GitConfiguration GitConfiguration { get; set; }
+	}
+}
