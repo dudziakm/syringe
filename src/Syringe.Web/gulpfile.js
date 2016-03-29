2d8329f1b5bd7fffff3b1e155633d01acfaa9350
Chris S <mrshrinkray@gmail.com>
2016-03-29 17:57:20 +0100

    Update gulpfile.js

--- src/Syringe.Web/gulpfile.js
+++ src/Syringe.Web/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='sass' />
+﻿/// <binding AfterBuild='default' />
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -6,7 +6,7 @@ var cleanCss = require('gulp-clean-css');
 var del = require('del');
 var vinylPaths = require('vinyl-paths');
 
-gulp.task('sass', function () {
+gulp.task('default', function () {
 	return gulp.src('./css/Syringe.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(vinylPaths(del))
