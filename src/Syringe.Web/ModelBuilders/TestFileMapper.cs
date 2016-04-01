8710d66e268733c43c3c8b8ec84ebb3eb58d4874
Hemang Rajyaguru <hemdagem@gmail.com>
2016-04-01 22:58:02 +0100

    cleanup

--- src/Syringe.Web/ModelBuilders/TestFileMapper.cs
+++ src/Syringe.Web/ModelBuilders/TestFileMapper.cs
@@ -7,92 +7,80 @@ using HeaderItem = Syringe.Core.Tests.HeaderItem;
 
 namespace Syringe.Web.ModelBuilders
 {
-	public class TestFileMapper : ITestFileMapper
-	{
-		public TestViewModel BuildViewModel(Test test)
-		{
-			if (test == null)
-			{
-				throw new ArgumentNullException(nameof(test));
-			}
+    public class TestFileMapper : ITestFileMapper
+    {
+        public TestViewModel BuildViewModel(Test test)
+        {
+            if (test == null)
+            {
+                throw new ArgumentNullException(nameof(test));
+            }
 
-			var verifications = new List<AssertionViewModel>();
-			IEnumerable<AssertionViewModel> verifyPositives = GetVerificationItems(test.Assertions);
+            var verifications = new List<AssertionViewModel>();
+            var verifyPositives = test.Assertions.Select(x => new AssertionViewModel { Regex = x.Regex, Description = x.Description, AssertionType = x.AssertionType });
 
-			verifications.AddRange(verifyPositives);
+            verifications.AddRange(verifyPositives);
 
-			var headerList = new List<Models.HeaderItem>(test.Headers.Select(x => new Models.HeaderItem { Key = x.Key, Value = x.Value }));
-			var capturedVariables = new List<Models.CapturedVariableItem>(test.CapturedVariables.Select(x => new Models.CapturedVariableItem { Description = x.Name, Regex = x.Regex }));
+            var headerList = new List<Models.HeaderItem>(test.Headers.Select(x => new Models.HeaderItem { Key = x.Key, Value = x.Value }));
+            var capturedVariables = new List<CapturedVariableItem>(test.CapturedVariables.Select(x => new CapturedVariableItem { Description = x.Name, Regex = x.Regex }));
 
-			var model = new TestViewModel
-			{
-				Position = test.Position,
-				ErrorMessage = test.ErrorMessage,
-				Headers = headerList,
-				LongDescription = test.LongDescription,
-				CapturedVariables = capturedVariables,
-				PostBody = test.PostBody,
-				PostType = test.PostType == PostType.GET.ToString() ? PostType.GET : PostType.POST,
-				VerifyResponseCode = test.VerifyResponseCode,
-				ShortDescription = test.ShortDescription,
-				Url = test.Url,
-				Assertions = verifications,
-				Filename = test.Filename,
-                AvailableVariables = test.AvailableVariables.Select(x=> new VariableViewModel {Name = x.Name,Value = x.Value}).ToList()
-			};
+            var model = new TestViewModel
+            {
+                Position = test.Position,
+                ErrorMessage = test.ErrorMessage,
+                Headers = headerList,
+                LongDescription = test.LongDescription,
+                CapturedVariables = capturedVariables,
+                PostBody = test.PostBody,
+                PostType = (PostType)Enum.Parse(typeof(PostType), test.PostType),
+                VerifyResponseCode = test.VerifyResponseCode,
+                ShortDescription = test.ShortDescription,
+                Url = test.Url,
+                Assertions = verifications,
+                Filename = test.Filename,
+                AvailableVariables = test.AvailableVariables.Select(x => new VariableViewModel { Name = x.Name, Value = x.Value }).ToList()
+            };
 
-			return model;
-		}
+            return model;
+        }
 
-		private IEnumerable<AssertionViewModel> GetVerificationItems(IEnumerable<Assertion> items)
-		{
-			return
-				items.Select(x =>
-						new AssertionViewModel
-						{
-							Regex = x.Regex,
-							Description = x.Description,
-							AssertionType = x.AssertionType
-						});
-		}
+        public IEnumerable<TestViewModel> BuildTests(IEnumerable<Test> tests)
+        {
+            if (tests == null)
+            {
+                throw new ArgumentNullException(nameof(tests));
+            }
 
-		public IEnumerable<TestViewModel> BuildTests(IEnumerable<Test> tests)
-		{
-			if (tests == null)
-			{
-				throw new ArgumentNullException(nameof(tests));
-			}
+            return tests.Select(x => new TestViewModel()
+            {
+                Position = x.Position,
+                ShortDescription = x.ShortDescription,
+                Url = x.Url
+            });
+        }
 
-			return tests.Select(x => new TestViewModel()
-			{
-				Position = x.Position,
-				ShortDescription = x.ShortDescription,
-				Url = x.Url
-			});
-		}
+        public Test BuildCoreModel(TestViewModel testModel)
+        {
+            if (testModel == null)
+            {
+                throw new ArgumentNullException(nameof(testModel));
+            }
 
-		public Test BuildCoreModel(TestViewModel testModel)
-		{
-			if (testModel == null)
-			{
-				throw new ArgumentNullException(nameof(testModel));
-			}
-
-			return new Test
-			{
-				Position = testModel.Position,
-				ErrorMessage = testModel.ErrorMessage,
-				Headers = testModel.Headers.Select(x => new HeaderItem(x.Key, x.Value)).ToList(),
-				LongDescription = testModel.LongDescription,
-				Filename = testModel.Filename,
-				CapturedVariables = testModel.CapturedVariables.Select(x => new CapturedVariable(x.Description, x.Regex)).ToList(),
-				PostBody = testModel.PostBody,
-				Assertions = testModel.Assertions.Select(x => new Assertion(x.Description, x.Regex, x.AssertionType)).ToList(),
-				ShortDescription = testModel.ShortDescription,
-				Url = testModel.Url,
-				PostType = testModel.PostType.ToString(),
-				VerifyResponseCode = testModel.VerifyResponseCode,
-			};
-		}
-	}
+            return new Test
+            {
+                Position = testModel.Position,
+                ErrorMessage = testModel.ErrorMessage,
+                Headers = testModel.Headers.Select(x => new HeaderItem(x.Key, x.Value)).ToList(),
+                LongDescription = testModel.LongDescription,
+                Filename = testModel.Filename,
+                CapturedVariables = testModel.CapturedVariables.Select(x => new CapturedVariable(x.Description, x.Regex)).ToList(),
+                PostBody = testModel.PostBody,
+                Assertions = testModel.Assertions.Select(x => new Assertion(x.Description, x.Regex, x.AssertionType)).ToList(),
+                ShortDescription = testModel.ShortDescription,
+                Url = testModel.Url,
+                PostType = testModel.PostType.ToString(),
+                VerifyResponseCode = testModel.VerifyResponseCode,
+            };
+        }
+    }
 }
