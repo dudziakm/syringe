5d75c4a59c87f143df61ab3cbf8c48e1310c2cc1
Chris S <mrshrinkray@gmail.com>
2016-04-02 18:15:04 +0100

    Moved to https://www.nuget.org/packages/SignalrTypescriptGenerator/ for the Signalr Typescript bindings, removed the t4 templates

--- src/Syringe.Web/Scripts/typings/Hubs.d.ts
+++ src/Syringe.Web/Scripts/typings/Hubs.d.ts
@@ -1,233 +1,187 @@
-﻿// Get signalr.d.ts.ts from https://github.com/borisyankov/DefinitelyTyped (or delete the reference)
+// Get signalr.d.ts.ts from https://github.com/borisyankov/DefinitelyTyped (or delete the reference)
 /// <reference path="signalr/signalr.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 
-////////////////////
-// available hubs //
-////////////////////
-//#region available hubs
-
+// Hubs
 interface SignalR
 {
-
-    /**
-      * The hub implemented by Syringe.Service.Api.Hubs.TaskMonitorHub
-      */
-    taskMonitorHub : Syringe.Service.Api.Hubs.TaskMonitorHub;
-}
-//#endregion available hubs
-
-///////////////////////
-// Service Contracts //
-///////////////////////
-//#region service contracts
-
-//#region TaskMonitorHub hub
-
-declare module Syringe.Service.Api.Hubs {
-interface TaskMonitorHub {
-    
-    /**
-      * This property lets you send messages to the TaskMonitorHub hub.
-      */
-    server : Syringe.Service.Api.Hubs.TaskMonitorHubServer;
-
-    /**
-      * The functions on this property should be replaced if you want to receive messages from the TaskMonitorHub hub.
-      */
-    client : Syringe.Service.Api.Hubs.ITaskMonitorHubClient;
+	taskMonitorHub : Syringe.Service.Api.Hubs.TaskMonitorHub;
 }
 
-interface TaskMonitorHubServer {
-
-    /** 
-      * Sends a "startMonitoringTask" message to the TaskMonitorHub hub.
-      * Contract Documentation: ---
-      * @param taskId {number} 
-      * @return {JQueryPromise of Syringe.Service.Api.Hubs.TaskState}
-      */
-    startMonitoringTask(taskId : number) : JQueryPromise<Syringe.Service.Api.Hubs.TaskState>;
-}
-} // end module
+// Service contracts
 
 declare module Syringe.Service.Api.Hubs
 {
-interface ITaskMonitorHubClient
+
+	interface TaskMonitorHub
+	{
+		server : Syringe.Service.Api.Hubs.TaskMonitorHubServer;
+		client : Syringe.Service.Api.Hubs.ITaskMonitorHubClient;
+	}
+
+	interface TaskMonitorHubServer
+	{
+		startMonitoringTask(taskId : number) : JQueryPromise<Syringe.Service.Api.Hubs.TaskState>;
+	}
+}
+
+
+// Clients
+
+declare module Syringe.Service.Api.Hubs
 {
-
-    /**
-      * Set this function with a "function(taskInfo : Syringe.Service.Api.Hubs.CompletedTaskInfo){}" to receive the "onTaskCompleted" message from the TaskMonitorHub hub.
-      * Contract Documentation: ---
-      * @param taskInfo {Syringe.Service.Api.Hubs.CompletedTaskInfo} 
-      * @return {void}
-      */
-    onTaskCompleted : (taskInfo : Syringe.Service.Api.Hubs.CompletedTaskInfo) => void;
-}
+	interface ITaskMonitorHubClient
+	{
+		onTaskCompleted : (taskInfo : Syringe.Service.Api.Hubs.CompletedTaskInfo) => void;
+	}
 }
 
-//#endregion TaskMonitorHub hub
 
-//#endregion service contracts
+// Data contracts
 
-
-
-////////////////////
-// Data Contracts //
-////////////////////
-//#region data contracts
-
-
-/**
-  * Data contract for Syringe.Service.Api.Hubs.CompletedTaskInfo
-  */
-declare module Syringe.Service.Api.Hubs {
-interface CompletedTaskInfo {
-    ActualUrl : string;
-    ResultId : number;
-    Success : boolean;
-    HttpResponse : Syringe.Core.Http.HttpResponse;
-    Position : number;
-    ExceptionMessage : string;
-    Verifications : Syringe.Core.Tests.Assertion[];
+declare module Syringe.Service.Api.Hubs
+{
+	interface CompletedTaskInfo
+	{
+		ActualUrl : string;
+		ResultId : number;
+		Success : boolean;
+		HttpResponse : Syringe.Core.Http.HttpResponse;
+		Position : number;
+		ExceptionMessage : string;
+		Verifications : Syringe.Core.Tests.Assertion[];
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for Syringe.Core.Tests.Assertion
-  */
-declare module Syringe.Core.Tests {
-interface Assertion {
-    Description : string;
-    Regex : string;
-    TransformedRegex : string;
-    Success : boolean;
-    AssertionType : Syringe.Core.Tests.AssertionType;
-    Log : string;
+declare module Syringe.Core.Tests
+{
+	interface Assertion
+	{
+		Description : string;
+		Regex : string;
+		TransformedRegex : string;
+		Success : boolean;
+		AssertionType : Syringe.Core.Tests.AssertionType;
+		Log : string;
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for Syringe.Core.Http.HttpResponse
-  */
-declare module Syringe.Core.Http {
-interface HttpResponse {
-    StatusCode : System.Net.HttpStatusCode;
-    Content : string;
-    Headers : System.Collections.Generic.KeyValuePair_String_String_[];
-    ResponseTime : System.TimeSpan;
+declare module Syringe.Core.Http
+{
+	interface HttpResponse
+	{
+		StatusCode : System.Net.HttpStatusCode;
+		Content : string;
+		Headers : System.Collections.Generic.KeyValuePair_String_String_[];
+		ResponseTime : System.TimeSpan;
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for System.TimeSpan
-  */
-declare module System {
-interface TimeSpan {
-    Ticks : number;
-    Days : number;
-    Hours : number;
-    Milliseconds : number;
-    Minutes : number;
-    Seconds : number;
-    TotalDays : number;
-    TotalHours : number;
-    TotalMilliseconds : number;
-    TotalMinutes : number;
-    TotalSeconds : number;
+declare module System
+{
+	interface TimeSpan
+	{
+		Ticks : number;
+		Days : number;
+		Hours : number;
+		Milliseconds : number;
+		Minutes : number;
+		Seconds : number;
+		TotalDays : number;
+		TotalHours : number;
+		TotalMilliseconds : number;
+		TotalMinutes : number;
+		TotalSeconds : number;
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for System.Collections.Generic.KeyValuePair`2[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089],[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]
-  */
-declare module System.Collections.Generic {
-interface KeyValuePair_String_String_ {
-    Key : string;
-    Value : string;
+declare module System.Collections.Generic
+{
+	interface KeyValuePair_String_String_
+	{
+		Key : string;
+		Value : string;
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for Syringe.Service.Api.Hubs.TaskState
-  */
-declare module Syringe.Service.Api.Hubs {
-interface TaskState {
-    TotalTests : number;
+declare module Syringe.Service.Api.Hubs
+{
+	interface TaskState
+	{
+		TotalTests : number;
+	}
 }
-} // end module
 
 
+// Enums
 
-/**
-  * Data contract for System.Net.HttpStatusCode
-  */
-declare module System.Net {
-enum HttpStatusCode {
-    Continue = 100,
-    SwitchingProtocols = 101,
-    OK = 200,
-    Created = 201,
-    Accepted = 202,
-    NonAuthoritativeInformation = 203,
-    NoContent = 204,
-    ResetContent = 205,
-    PartialContent = 206,
-    MultipleChoices = 300,
-    Ambiguous = 300,
-    MovedPermanently = 301,
-    Moved = 301,
-    Found = 302,
-    Redirect = 302,
-    SeeOther = 303,
-    RedirectMethod = 303,
-    NotModified = 304,
-    UseProxy = 305,
-    Unused = 306,
-    TemporaryRedirect = 307,
-    RedirectKeepVerb = 307,
-    BadRequest = 400,
-    Unauthorized = 401,
-    PaymentRequired = 402,
-    Forbidden = 403,
-    NotFound = 404,
-    MethodNotAllowed = 405,
-    NotAcceptable = 406,
-    ProxyAuthenticationRequired = 407,
-    RequestTimeout = 408,
-    Conflict = 409,
-    Gone = 410,
-    LengthRequired = 411,
-    PreconditionFailed = 412,
-    RequestEntityTooLarge = 413,
-    RequestUriTooLong = 414,
-    UnsupportedMediaType = 415,
-    RequestedRangeNotSatisfiable = 416,
-    ExpectationFailed = 417,
-    UpgradeRequired = 426,
-    InternalServerError = 500,
-    NotImplemented = 501,
-    BadGateway = 502,
-    ServiceUnavailable = 503,
-    GatewayTimeout = 504,
-    HttpVersionNotSupported = 505,
+declare module System.Net
+{
+	enum HttpStatusCode
+	{
+		Continue = 100,
+		SwitchingProtocols = 101,
+		OK = 200,
+		Created = 201,
+		Accepted = 202,
+		NonAuthoritativeInformation = 203,
+		NoContent = 204,
+		ResetContent = 205,
+		PartialContent = 206,
+		MultipleChoices = 300,
+		Ambiguous = 300,
+		MovedPermanently = 301,
+		Moved = 301,
+		Found = 302,
+		Redirect = 302,
+		SeeOther = 303,
+		RedirectMethod = 303,
+		NotModified = 304,
+		UseProxy = 305,
+		Unused = 306,
+		TemporaryRedirect = 307,
+		RedirectKeepVerb = 307,
+		BadRequest = 400,
+		Unauthorized = 401,
+		PaymentRequired = 402,
+		Forbidden = 403,
+		NotFound = 404,
+		MethodNotAllowed = 405,
+		NotAcceptable = 406,
+		ProxyAuthenticationRequired = 407,
+		RequestTimeout = 408,
+		Conflict = 409,
+		Gone = 410,
+		LengthRequired = 411,
+		PreconditionFailed = 412,
+		RequestEntityTooLarge = 413,
+		RequestUriTooLong = 414,
+		UnsupportedMediaType = 415,
+		RequestedRangeNotSatisfiable = 416,
+		ExpectationFailed = 417,
+		UpgradeRequired = 426,
+		InternalServerError = 500,
+		NotImplemented = 501,
+		BadGateway = 502,
+		ServiceUnavailable = 503,
+		GatewayTimeout = 504,
+		HttpVersionNotSupported = 505,
+	}
 }
-} // end module
 
 
-/**
-  * Data contract for Syringe.Core.Tests.AssertionType
-  */
-declare module Syringe.Core.Tests {
-enum AssertionType {
-    Negative = 0,
-    Positive = 1,
+declare module Syringe.Core.Tests
+{
+	enum AssertionType
+	{
+		Negative = 0,
+		Positive = 1,
+	}
 }
-} // end module
 
-//#endregion data contracts
 
