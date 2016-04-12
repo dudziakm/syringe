82515f272004f5f02d7be28c85b7ee44310d3b3b
Chris S. <mrshrinkray@gmail.com>
2016-04-12 21:42:32 +0100

    Added delete confirmations for files and tests, changed the confirms to use Bootbox.js

--- src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
+++ src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿/// <reference path="../typings/bootbox.d.ts" />
+$(document).ready(function ()
+{
     const rowsToAdd = [
         { $Button: $("#addVerification"), URL: "/Test/AddAssertion", Prefix: "Assertions" },
         { $Button: $("#addParsedItem"), URL: "/Test/AddCapturedVariableItem", Prefix: "CapturedVariables" },
@@ -12,4 +14,15 @@
         e.preventDefault();
         $(this).closest(".form-group").remove();
     });
+
+	$(".delete-button").on("click", function()
+	{
+		var success = false;
+		bootbox.confirm("Are you sure you want delete?", function (result)
+		{
+			success = result;
+		});
+
+		return success;
+	});
 });
