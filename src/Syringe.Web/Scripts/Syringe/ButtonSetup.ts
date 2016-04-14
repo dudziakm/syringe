30ad43c1feb9838590d5308fb3f46b875b349490
Hemang Rajyaguru <hemdagem@gmail.com>
2016-04-14 22:22:34 +0100

    fix for headers,variables, assertions breaking editors
    Reset variable names with correct index so editors don't crash.

--- src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
+++ src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
@@ -1,6 +1,5 @@
 ﻿/// <reference path="../typings/bootbox.d.ts" />
-$(document).ready(function ()
-{
+$(document).ready(function () {
     const rowsToAdd = [
         { $Button: $("#addVerification"), URL: "/Test/AddAssertion", Prefix: "Assertions" },
         { $Button: $("#addParsedItem"), URL: "/Test/AddCapturedVariableItem", Prefix: "CapturedVariables" },
@@ -12,17 +11,32 @@ $(document).ready(function ()
 
     $("body").on("click", "#removeRow", function (e) {
         e.preventDefault();
+        var formGroup = $(this).closest(".form-group");
+        var parentPanelBody = formGroup.closest(".panel-body");
         $(this).closest(".form-group").remove();
+
+        parentPanelBody.find(".form-group").each(function (i, ev) {
+            $(ev).find("label").each(function () {
+                rowHandler.updateElementValue($(this), i, "for");
+            });
+
+            $(ev).find("input, select").each(function () {
+
+                rowHandler.updateElementValue($(this), i, "name");
+                rowHandler.updateElementValue($(this), i, "id");
+
+            });
+
+        });
+
     });
 
-	$(".delete-button").on("click", function()
-	{
-		var success = false;
-		bootbox.confirm("Are you sure you want delete?", function (result)
-		{
-			success = result;
-		});
+    $(".delete-button").on("click", function () {
+        var success = false;
+        bootbox.confirm("Are you sure you want delete?", function (result) {
+            success = result;
+        });
 
-		return success;
-	});
+        return success;
+    });
 });
