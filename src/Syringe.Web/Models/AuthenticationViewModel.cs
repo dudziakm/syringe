45088259b2d3554cc779173c5df45ebe3e83ab83
Chris Small <chris.small@totaljobsgroup.com>
2016-04-15 14:30:08 +0100

    Added warning message on the login page when there are no configured OAuth providers

--- src/Syringe.Web/Models/AuthenticationViewModel.cs
+++ src/Syringe.Web/Models/AuthenticationViewModel.cs
@@ -6,5 +6,15 @@ namespace Syringe.Web.Models
     {
         public IConfiguration Configuration { get; set; }
         public string ReturnUrl { get; set; }
+
+	    public bool IsOAuthConfigEmpty
+	    {
+		    get
+		    {
+			    return string.IsNullOrEmpty(Configuration.OAuthConfiguration.MicrosoftAuthClientId) &&
+			           string.IsNullOrEmpty(Configuration.OAuthConfiguration.GoogleAuthClientId) &&
+			           string.IsNullOrEmpty(Configuration.OAuthConfiguration.GithubAuthClientId);
+		    }
+	    }
     }
 }
